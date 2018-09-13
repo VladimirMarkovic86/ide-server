132e6db2092f11a6b8c0cde8650114d26a6f3ebf
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-13 18:12:14 +0200

    Formatting code.

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure	"1.9.0"]
-         								[org.vladimir/server-lib "0.1.0"]
-         								[org.vladimir/mongo-lib "0.1.0"]
-         								[org.vladimir/utils-lib "0.1.0"]
-         								[org.vladimir/ajax-lib "0.1.0"]
-         								[org.vladimir/session-lib "0.1.0"]
-         								[org.vladimir/ide-middle "0.1.0"]
-         								[org.vladimir/language-lib "0.1.0"]
-         								[org.vladimir/common-server "0.1.0"]
-         								]
-  
+                 [org.vladimir/server-lib "0.1.0"]
+                 [org.vladimir/mongo-lib "0.1.0"]
+                 [org.vladimir/utils-lib "0.1.0"]
+                 [org.vladimir/ajax-lib "0.1.0"]
+                 [org.vladimir/session-lib "0.1.0"]
+                 [org.vladimir/ide-middle "0.1.0"]
+                 [org.vladimir/language-lib "0.1.0"]
+                 [org.vladimir/common-server "0.1.0"]
+                 ]
+
   ; AOT - Compailation ahead of time
   :main ^:skip-aot ide-server.core
   :target-path "target/%s"
