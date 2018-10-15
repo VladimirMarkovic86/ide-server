e719f6952b3473f8be1aae9522759ff4185472ee
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:45:41 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,19 +1,21 @@
-(defproject org.vladimir/ide-server "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/ide-server "0.1.0"
   :description "Integrated development environment server"
-  :url "http://gitlab:1610/VladimirMarkovic86/ide-server"
+  :url "http://github.com/VladimirMarkovic86/ide-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.vladimir/server-lib "0.1.0"]
-                 [org.vladimir/db-lib "0.1.0"]
-                 [org.vladimir/utils-lib "0.1.0"]
-                 [org.vladimir/ajax-lib "0.1.0"]
-                 [org.vladimir/session-lib "0.1.0"]
-                 [org.vladimir/ide-middle "0.1.0"]
-                 [org.vladimir/language-lib "0.1.0"]
-                 [org.vladimir/common-server "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/db-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/ide-middle "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.1.0"]
                  ]
 
+  :min-lein-version "2.0.0"
+  
   ; AOT - Compailation ahead of time
   :main ^:skip-aot ide-server.core
   :target-path "target/%s"
