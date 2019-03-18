af7d57c9f529b5ec7472b3c50792e5d6e391af5e
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-18 16:26:22 +0100

    Task 63: Fixed optimization advanced compiling

--- project.clj
+++ project.clj
@@ -11,8 +11,8 @@
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.13"]
                  [org.clojars.vladimirmarkovic86/ide-middle "0.2.10"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.20"]
-                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.16"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.21"]
+                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.17"]
                  ]
 
   :min-lein-version "2.0.0"
